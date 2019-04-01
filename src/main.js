ff508319bd6761d7264a1865e870ce4c927b4495
黄建文 <huangjianwen@thmarket.cn>
2019-04-01 15:47:40 +0800

    swiper and grid

--- src/main.js
+++ src/main.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 
+//导入路由的包
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//导入vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 //导入MUI样式
 import './lib/mui/css/mui.min.css'
 
+import './lib/mui/css/icons-extra.css'
+
 //按需导入Mint-UI中的组件
-import { Header } from 'mint-ui'
+import { Header,Swipe,SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name,Swipe)
+Vue.component(SwipeItem.name,SwipeItem)
+
+
+//导入路由模块
+import router from './router.js'
+
 //导入app组件
 import app from './App.vue'
 
 var vm = new Vue({
     el:'#app',
-    render: c => c(app)
+    render: c => c(app),
+    router
 })
